ce3645cdfe5c525ae3c7d76d9148903742ca3022
MengHsiu Chang <shou7783@gmail.com>
2017-04-05 21:26:26 +0800

    add simple mapView

---
+++ kapi-kaffeine/kapi-kaffeine/KKMapViewController.swift
@@ -0,0 +1,53 @@
+//
+//  KKMapViewController.swift
+//  kapi-kaffeine
+//
+//  Created by MengHsiu Chang on 05/04/2017.
+//  Copyright © 2017 kapi-kaffeine. All rights reserved.
+//
+
+import UIKit
+import GoogleMaps
+
+class KKMapViewController: UIViewController {
+    
+
+    override func loadView() {
+        let camera = GMSCameraPosition.camera(withLatitude: 25.018744, longitude: 121.532785, zoom: 18.0)
+        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        
+        do {
+            // Set the map style by passing the URL of the local file.
+            if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
+                mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+            } else {
+                NSLog("Unable to find style.json")
+            }
+        } catch {
+            NSLog("One or more of the map styles failed to load. \(error)")
+        }
+        
+        let position = CLLocationCoordinate2DMake(25.018744, 121.532785)
+        let london = GMSMarker(position: position)
+        london.icon = UIImage(named: "icon_mapMarker")
+        london.map = mapView;
+        
+        self.view = mapView
+    }
+    
+    
+//    override func loadView() {
+//        // Create a GMSCameraPosition that tells the map to display the
+//        // coordinate -33.86,151.20 at zoom level 6.
+//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
+//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+//        view = mapView
+//        
+//        // Creates a marker in the center of the map.
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
+//    }
+}
