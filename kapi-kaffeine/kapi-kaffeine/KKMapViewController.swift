adb91e5adb4cefefaa561ececa2c53b760ef9ef1
MengHsiu Chang <shou7783@gmail.com>
2017-04-06 10:01:36 +0800

    load cafe data

--- kapi-kaffeine/kapi-kaffeine/KKMapViewController.swift
+++ kapi-kaffeine/kapi-kaffeine/KKMapViewController.swift
@@ -9,12 +9,15 @@
 import UIKit
 import GoogleMaps
 
-class KKMapViewController: UIViewController {
+class KKMapViewController: UIViewController, GMSMapViewDelegate {
     
-
+    var cafeData: Array<NSDictionary> = []
+    
     override func loadView() {
         let camera = GMSCameraPosition.camera(withLatitude: 25.018744, longitude: 121.532785, zoom: 18.0)
         let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        
+        mapView.delegate = self
         
         do {
             // Set the map style by passing the URL of the local file.
@@ -29,25 +32,49 @@ class KKMapViewController: UIViewController {
         
         let position = CLLocationCoordinate2DMake(25.018744, 121.532785)
         let london = GMSMarker(position: position)
+        london.title = "london"
         london.icon = UIImage(named: "icon_mapMarker")
         london.map = mapView;
         
         self.view = mapView
     }
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let dataURL = Bundle.main.url(forResource: "cafes", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: dataURL)
+                self.cafeData = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as! Array<NSDictionary>
+                (self.view as! GMSMapView).clear()
+                
+                for cafe in self.cafeData {
+                    let position = CLLocationCoordinate2DMake(Double(cafe.object(forKey: "latitude") as! String)!,
+                                                              Double(cafe.object(forKey: "longitude") as! String)!)
+                    print(position)
+                    let marker = GMSMarker(position: position)
+                    marker.title = cafe.object(forKey: "name") as? String
+                    marker.icon = UIImage(named: "icon_mapMarker")
+                    marker.map = self.view as? GMSMapView
+                    
+                }
+                
+            } catch {
+                
+            }
+            
+            
+        }
+        
+    }
     
-//    override func loadView() {
-//        // Create a GMSCameraPosition that tells the map to display the
-//        // coordinate -33.86,151.20 at zoom level 6.
-//        let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-//        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
-//        view = mapView
-//        
-//        // Creates a marker in the center of the map.
-//        let marker = GMSMarker()
-//        marker.position = CLLocationCoordinate2D(latitude: -33.86, longitude: 151.20)
-//        marker.title = "Sydney"
-//        marker.snippet = "Australia"
-//        marker.map = mapView
-//    }
+    func mapView(_ mapView: GMSMapView, markerInfoWindow marker: GMSMarker) -> UIView? {
+        marker.icon = UIImage(named: "icon_mapMarkerSelected")
+        return nil
+    }
+    
+    func mapView(_ mapView: GMSMapView, didCloseInfoWindowOf marker: GMSMarker) {
+        marker.icon = UIImage(named: "icon_mapMarker")
+    }
+    
 }
