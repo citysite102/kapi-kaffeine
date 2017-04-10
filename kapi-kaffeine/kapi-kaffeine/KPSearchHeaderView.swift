0dbc9708bfc9b7a4864c1a37c077974804dd3e4c
YU CHONKAO <Sameul@Samuel.local>
2017-04-10 20:21:13 +0800

    Update Structure

---
+++ kapi-kaffeine/kapi-kaffeine/KPSearchHeaderView.swift
@@ -0,0 +1,21 @@
+//
+//  KPSearchHeaderView.swift
+//  kapi-kaffeine
+//
+//  Created by YU CHONKAO on 2017/4/10.
+//  Copyright © 2017年 kapi-kaffeine. All rights reserved.
+//
+
+import UIKit
+
+class KPSearchHeaderView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
