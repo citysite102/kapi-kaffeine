4ffc517d9d4c926777afb95536f0ec0608c88a3f
Samuel <citysite1025@gmail.com>
2017-05-14 23:03:24 +0800

    Set up information

--- kapi-kaffeine/kapi-kaffeine/KPPhotoDisplayCell.swift
+++ kapi-kaffeine/kapi-kaffeine/KPPhotoDisplayCell.swift
@@ -11,12 +11,35 @@ import UIKit
 class KPPhotoDisplayCell: UICollectionViewCell {
     
     var shopPhoto:UIImageView!
+    lazy var photoTitleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20.0)
+        label.textColor = KPColorPalette.KPTextColor.whiteColor
+        return label
+    }()
+    lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14.0)
+        label.textColor = KPColorPalette.KPTextColor.whiteColor
+        return label
+    }()
+    
     
     override init(frame: CGRect) {
         super.init(frame: frame);
         
         self.backgroundColor = UIColor.black;
         self.alpha = 0.4;
+        
+        addSubview(photoTitleLabel)
+        photoTitleLabel.text = "覺旅咖啡 Journey Cafe"
+        photoTitleLabel.addConstraintForCenterAligningToSuperview(in: .horizontal)
+        photoTitleLabel.addConstraint(from: "V:|-16-[$self]")
+        
+        addSubview(countLabel)
+        countLabel.text = "6 of 104"
+        countLabel.addConstraintForCenterAligningToSuperview(in: .horizontal)
+        countLabel.addConstraint(from: "V:[$self]-32-|")
         
         self.shopPhoto = UIImageView();
         self.shopPhoto.layer.cornerRadius = 2.0;
