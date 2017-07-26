d040377b63a29f2beafac0937a23f1bc548f80f8
MengHsiu Chang <shou7783@gmail.com>
2017-07-26 19:19:43 +0800

    update google map style

--- kapi-kaffeine/kapi-kaffeine/KPMainMapViewController.swift
+++ kapi-kaffeine/kapi-kaffeine/KPMainMapViewController.swift
@@ -190,16 +190,16 @@ GMUClusterRendererDelegate {
         mapView.delegate = self
         mapView.preferredFrameRate = .maximum
         
-//        do {
-//            // Set the map style by passing the URL of the local file.
-//            if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
-//                mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
-//            } else {
-//                NSLog("Unable to find style.json")
-//            }
-//        } catch {
-//            NSLog("One or more of the map styles failed to load. \(error)")
-//        }
+        do {
+            // Set the map style by passing the URL of the local file.
+            if let styleURL = Bundle.main.url(forResource: "style", withExtension: "json") {
+                mapView.mapStyle = try GMSMapStyle(contentsOfFileURL: styleURL)
+            } else {
+                NSLog("Unable to find style.json")
+            }
+        } catch {
+            NSLog("One or more of the map styles failed to load. \(error)")
+        }
         
         self.view.addSubview(self.mapView)
         
